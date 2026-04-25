7bf001d2c0aee88c7ddc00611f84a9874168d390
Ismail <ismail.shalanfeh@gmail.com>
2026-04-25 15:00:38 -0700

    Timer Changes

--- Scenes/Timer/timer.gd
+++ Scenes/Timer/timer.gd
@@ -1,18 +1,23 @@
 extends TextureProgressBar
 
 @export var TimeKeeper: Timer
-@export var TimeLeft: int
+
+#for smoothing
+@export var Multiplier: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	max_value = TimeLeft
-	TimeKeeper.start()
+	max_value = Globals.GameValues.TimeLeft * Multiplier
+	value = Globals.GameValues.TimeLeft * Multiplier
+	
+	TimeKeeper.wait_time = Globals.GameValues.TickSpeed
 	TimeKeeper.timeout.connect(Timeout)
-	print("hello")
+	TimeKeeper.start()
 
 func Timeout() -> void:
-	print("beep")
-	TimeLeft -= 1
-	value = TimeLeft
-	if TimeLeft <= 0:
-		TimeKeeper.stop()
+	Globals.GameValues.TimeLeft -= 1
+	TimeKeeper.wait_time = Globals.GameValues.TickSpeed
+	Globals.TimeChange.emit()
+
+func _process(delta: float) -> void:
+	value = lerp(value, Globals.GameValues.TimeLeft*10, 0.1)
