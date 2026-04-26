08c2a751b325e1521662108fa5b17173af01f90e
Ismail <ismail.shalanfeh@gmail.com>
2026-04-26 16:36:31 -0700

    Update APIKeys.gd

--- Code/APIKeys.gd
+++ Code/APIKeys.gd
@@ -1,7 +1,7 @@
 extends Node
 class_name APIKeys
 
-var AIKey: String = "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"
+var AIKey: String = "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"
 
 func _ready():
 	pass
